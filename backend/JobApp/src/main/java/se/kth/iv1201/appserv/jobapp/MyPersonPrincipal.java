9db0adeb6a1cd47e180852a2170b0f00fd6bbb0b
kristian <kfranzen@kth.se>
2023-02-03 18:18:42 +0100

    I hope it does not fuck

---
+++ backend/JobApp/src/main/java/se/kth/iv1201/appserv/jobapp/MyPersonPrincipal.java
@@ -0,0 +1,55 @@
+package se.kth.iv1201.appserv.jobapp;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import se.kth.iv1201.appserv.jobapp.domain.Person;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MyPersonPrincipal implements UserDetails {
+    private Person person;
+
+    public MyPersonPrincipal(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_" + person.getRole_id()));
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- backend/JobApp/src/main/java/se/kth/iv1201/appserv/jobapp/controller/UserController.java
+++ backend/JobApp/src/main/java/se/kth/iv1201/appserv/jobapp/controller/UserController.java
@@ -1,24 +1,28 @@
 package se.kth.iv1201.appserv.jobapp.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.kth.iv1201.appserv.jobapp.domain.external.request.UserDTO;
 import se.kth.iv1201.appserv.jobapp.domain.external.response.GenericResponse;
 import se.kth.iv1201.appserv.jobapp.service.UserService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/any")
 public class UserController {
 
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("admin")
+    public String admin(){
+        return "admin can log in here";
+    }
+    @GetMapping("any/g")
+    public String any(){
+        return "Anyone can log in here";
     }
 
     @PostMapping("login")
