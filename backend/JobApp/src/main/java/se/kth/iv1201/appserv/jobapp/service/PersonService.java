ca5e62d924241645d29c8128abd4121d40040d4e
nnayerii <107840993+nnayerii@users.noreply.github.com>
2023-02-03 18:18:09 +0100

    Added register users backend

--- backend/JobApp/src/main/java/se/kth/iv1201/appserv/jobapp/service/PersonService.java
+++ backend/JobApp/src/main/java/se/kth/iv1201/appserv/jobapp/service/PersonService.java
@@ -1,7 +1,10 @@
 package se.kth.iv1201.appserv.jobapp.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import se.kth.iv1201.appserv.jobapp.domain.Person;
+import se.kth.iv1201.appserv.jobapp.domain.Role;
+import se.kth.iv1201.appserv.jobapp.domain.external.request.PersonDTO;
 import se.kth.iv1201.appserv.jobapp.repository.PersonRepository;
 
 import java.util.List;
@@ -17,5 +20,12 @@ public class PersonService {
 
     public List<Person> getAllUsers(){
         return personRepository.findAll();
+    }
+
+    @Transactional
+    public void registerUser(PersonDTO personDTO){
+        Person person = new Person(personDTO.getFirstname(), personDTO.getLastname(), personDTO.getPersonnumber(),
+                personDTO.getEmailaddress(), personDTO.getPassword(), 2, personDTO.getUsername());
+        personRepository.save(person);
     }
 }
